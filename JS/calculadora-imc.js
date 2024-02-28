ef2284f2337f1508e521275f6d02c902cff62347
mateus <mateustessarocamargo@gmail.com>
2024-02-28 11:48:29 -0300

    tetas

--- JS/calculadora-imc.js
+++ JS/calculadora-imc.js
@@ -17,10 +17,27 @@ const formulario = document.querySelector('#formulario');
 
         const imc = calcularImc(peso, altura)
         console.log(imc)
+        const classificado = tabelaImc(imc)
+        console.log(classificado)
+        mostrarResultado(classificado)
     })
     function calcularImc(peso, altura){
         let imc = (peso / altura ** 2)
         return imc.toFixed(2);
+    }
+    function mostrarResultado(msg){
+        const resultado = document.querySelector("#resultado");
+        resultado.innerHTML = msg
+    }
+    function tabelaImc(imc){
+        const classificado = [' você está abaixo do peso', ' Seu peso está normal', ' você está com sobrepso', ' Você está teta direita do luiz', 'Você está teta esquerda do luiz', 'Você está Luiz teta']
+        if (imc >= 39.9) return classificado[5];
+        if (imc >= 35) return classificado[4];
+        if (imc >= 30) return classificado[3];
+        if (imc >= 25)  return classificado[2];
+        if (imc >= 18.5) return classificado[1];
+        if (imc < 18.5)return classificado[0];
+    
     }
 
         
